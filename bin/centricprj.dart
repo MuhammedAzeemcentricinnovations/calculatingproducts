6cab5337e1bfe8925e9f9054f9e50ff6b7d9aa4c
MuhammedAzeemcentricinnovations <azeemcentricinnovations@gmail.com>
2023-09-12 12:47:08 +0530

    created class pen and extends product class for calculating pen price

---
+++ bin/centricprj.dart
@@ -0,0 +1,74 @@
+
+
+
+abstract class Product {
+  Product(this.price, {this.weight, this.volume,  this.count});
+
+  int price;
+  int? weight;
+  int? volume;
+  int? count;
+  late int totalPrice;
+
+  void calculate();
+}
+
+class Pen extends Product {
+  Pen({required price, required count}) : super(price,count: count);
+  void computePenname() {
+   ;
+  }
+
+  @override
+  void calculate() {
+
+    if (count!=null){
+     totalPrice = price * count!;
+      print("3 pen price is$totalPrice Rupeess");
+
+    }
+    
+     
+    
+  }
+}
+
+
+// class Sugar extends Product{
+//   Sugar({ required price,required weight}):super(price,weight: weight);
+
+
+//   @override
+//   void calculate() {
+
+//     if (weight!=null){
+//      totalPrice=price * weight!;
+//       print(" 35 kg sugar price is $totalPrice Rs");
+//     }
+        
+//   }}
+
+
+//   class Oil extends Product{
+//   Oil ({ required price,required volume}):super(price,volume: volume);
+  
+//   @override
+//   void calculate() {
+//     if (volume!=null){
+//      totalPrice=price * volume!;
+//       print("10 liter oil price is $totalPrice Rs");
+//     }
+//   }
+// }
+
+
+void main() {
+  final prctprice= Pen(price: 12, count: 3);
+//   final prctweight=Sugar(price: 35, weight: 23);
+//   final prctvolume=Oil(price: 105, volume: 50);
+
+//   // prctprice.computePenname();
+  prctprice.calculate();
+//   prctweight.calculate();
+//   prctvolume.calculate();
+}
---
+++ test/centricprj_test.dart
@@ -0,0 +1,8 @@
+import 'package:centricprj/centricprj.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
